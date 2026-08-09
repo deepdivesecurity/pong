c992e74252b163e018d75dc479bc950d0b3d72ed
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-08-09 15:28:04 -0400

    full working game

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
 from scoreboard import Scoreboard
+from ball import Ball
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import time
 
 def main(): 
     screen = Screen()
@@ -15,6 +17,8 @@ def main():
 
     scoreboard = Scoreboard()
 
+    ball = Ball()
+
     screen.listen()
     screen.onkey(p1_paddle.up, "Up")
     screen.onkey(p1_paddle.down, "Down")
@@ -23,8 +27,34 @@ def main():
 
     game_on = True
     while game_on: 
+        time.sleep(0.1)
+        ball.move()
         screen.update()
 
+        # Check for collision with wall
+        if ball.xcor() > SCREEN_WIDTH / 2 + 80: 
+            # Update scoreboard
+            scoreboard.increase_score(2)
+            ball.reset()
+
+        # Check for collision with wall
+        if ball.xcor() < SCREEN_WIDTH / -2 - 80: 
+            # Update scoreboard
+            scoreboard.increase_score(1)
+            ball.reset()
+
+        # Check for collision with top or bottom wall
+        if ball.ycor() > SCREEN_HEIGHT / 2 or ball.ycor() < SCREEN_HEIGHT / -2: 
+            # Bounce ball away from wall
+            ball.bounce_y()
+
+        # Check for collision with paddle
+        if ball.distance(p1_paddle) < 50 and ball.xcor() > 320 or ball.distance(p2_paddle) < 50 and ball.xcor() < -320: 
+            ball.bounce_x()
+
+        # Check for collision with left paddle
+
+        
     screen.exitonclick()
 
 if __name__ == "__main__":
--- scoreboard.py
+++ scoreboard.py
@@ -8,22 +8,30 @@ COLOR = "red"
 class Scoreboard(Turtle): 
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.p1_score = 0
+        self.p2_score = 0
         self.color("white")
         self.penup()
         self.goto(0, SCREEN_HEIGHT / 2 - 40)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"P1: {self.p1_score}   P2: {self.p2_score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
         
     def update_scoreboard(self): 
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"P1: {self.p1_score}   P2: {self.p2_score}", align=ALIGNMENT, font=FONT)
 
-    def increase_score(self): 
-        self.score += 1
+    def increase_score(self, player): 
+        if player == 1: 
+            self.p1_score += 1
+        if player == 2: 
+            self.p2_score += 1
         self.update_scoreboard()
 
-    def game_over(self): 
-        self.goto(0, 0)
-        self.color(COLOR)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self): 
+    #     self.goto(0, 0)
+    #     self.color(COLOR)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    # def check_score(self): 
+    #     if self.p1_score == 2 or self.p2_score == 2: 
+    #         self.game_over()
